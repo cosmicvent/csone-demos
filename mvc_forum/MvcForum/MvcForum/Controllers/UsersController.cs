4043d098afa6dd2c7b12f17f6276069960c955cb
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-08-11 18:50:25 +0530

    added the signup stuff

---
+++ mvc_forum/MvcForum/MvcForum/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcForum.Models;
+
+namespace MvcForum.Controllers {
+    public class UsersController : Controller {
+
+        MvcForumDataContext _dataContext;
+
+        public UsersController() {
+            _dataContext = new MvcForumDataContext();
+        }
+
+        public ActionResult SignUp() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp( FormCollection form ) {
+            User user = new User();
+            user.UserName = form["username"];
+            user.Password = form["password"];
+            user.CreatedOn = DateTime.Now;
+            _dataContext.Users.InsertOnSubmit( user );
+            _dataContext.SubmitChanges();
+            TempData["success"] = "Registered user successfully";
+            return Redirect( "/" );
+        }
+
+    }
+}
---
+++ mvc_forum/MvcForum/MvcForum/Views/Users/SignUp.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>SignUp</title>
+</head>
+<body>
+    <div>
+        <form method='post'>
+        User Name<input type='text' name='username' /><br />
+        Password
+        <input type='password' name='password' /><br />
+        Confirm Password
+        <input type='password' name='confirmation' /><br />
+        <input type='submit' value='Sign Up' />
+        </form>
+    </div>
+</body>
+</html>
